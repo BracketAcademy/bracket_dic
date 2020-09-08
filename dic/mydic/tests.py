fd4d87437f4ea509bf4696b2ba68e04347fe799f
Arman Hadi <armanhadi728@yahoo.com>
2020-09-08 18:07:08 +0430

    test

--- dic/mydic/tests.py
+++ dic/mydic/tests.py
@@ -9,4 +9,14 @@ class wordModelTest(TestCase):
     def test_recent_future(self):
         time = timezone.now() + timedelta(seconds=1)
         objtest = word(word_date=time)
+        self.assertIs(objtest.recent(), False)
+
+    def test_recent_ok(self):
+        time = timezone.now() - timedelta(hours=11, minutes=59, seconds=59)
+        objtest = word(word_date=time)
         self.assertIs(objtest.recent(), True)
+
+    def test_recent_past(self):
+        time = timezone.now() - timedelta(hours=12, minutes=0, seconds=1)
+        objtest = word(word_date=time)
+        self.assertIs(objtest.recent(), False)
