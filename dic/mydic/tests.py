266d9d0989937e33d7bd8eda7ef9aeab78e0d48d
Arman Hadi <armanhadi728@yahoo.com>
2020-09-08 18:03:40 +0430

    test

--- dic/mydic/tests.py
+++ dic/mydic/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from mydic.models import word
+from django.utils import timezone
+from datetime import timedelta
+
 
 # Create your tests here.
+class wordModelTest(TestCase):
+    def test_recent_future(self):
+        time = timezone.now() + timedelta(seconds=1)
+        objtest = word(word_date=time)
+        self.assertIs(objtest.recent(), True)
