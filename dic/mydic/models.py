e425529ddd20dffb4d6f4ab25b20cd03ad0e9f48
Arman Hadi <armanhadi728@yahoo.com>
2020-09-09 12:33:17 +0430

    djangoadmin

--- dic/mydic/models.py
+++ dic/mydic/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 from datetime import timedelta
 # Create your models here.
 class word(models.Model):
-    word_text = models.CharField(max_length=50)
-    word_type = models.CharField(max_length=50)
+    word_text = models.CharField(max_length=50, verbose_name="Word")
+    word_type = models.CharField(max_length=50, verbose_name="Type")
 
-    word_trans = models.CharField(max_length=200)
-    word_date = models.DateTimeField("date added")
+    word_trans = models.CharField(max_length=200, verbose_name="Translation")
+    word_date = models.DateTimeField("Date Added")
 
     def recent(self):
         return timezone.now() - timedelta(hours=12) <= self.word_date <= timezone.now()
@@ -26,6 +26,5 @@ class word(models.Model):
         del a,b,c,
         '''
         
-
     def __str__(self):
         return self.word_text
