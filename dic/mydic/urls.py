8a645fcfa1f1ceae08b14f463adbd1d442983346
vsh1105 <vahid.shkar.304@gmail.com>
2020-09-10 11:58:09 +0430

    create form

--- dic/mydic/urls.py
+++ dic/mydic/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from mydic import views
-
+app_name='midic'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:w_id>/detail', views.detail, name="detail")
+    path('detail/<int:w_id>/', views.detail, name="detail")
 ]
