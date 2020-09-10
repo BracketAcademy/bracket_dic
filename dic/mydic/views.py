0dcdc4187ad6ee7af838ddf5e32fff7c0a2cad80
Arman Hadi <armanhadi728@yahoo.com>
2020-09-10 11:29:43 +0430

    order ok

--- dic/mydic/views.py
+++ dic/mydic/views.py
@@ -4,8 +4,10 @@ from .models import *
 # Create your views here.
 
 def index(req):
-    w = word.objects.all()
+    w = word.objects.order_by('word_text')
     return render(req, 'mydic/index.html', context={'word':w})
+
+
 def detail(req, w_id):
     w = word.objects.get(id=w_id)
     return render(req, 'mydic/detail.html', {'w':w})
