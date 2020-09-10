4f61f8615914ed7b2ecb1347734852ef00893e6f
Arman Hadi <armanhadi728@yahoo.com>
2020-09-10 12:33:53 +0430

    'form'

--- dic/mydic/urls.py
+++ dic/mydic/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from mydic import views
-app_name='midic'
+app_name='mydic'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('detail/<int:w_id>/', views.detail, name="detail")
+    path('detail/<int:w_id>/', views.detail, name="detail"),
+    path('form', views.form, name="form"),
+    path('submit', views.submit, name="submit")
 ]
--- dic/mydic/views.py
+++ dic/mydic/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -10,3 +13,17 @@ def index(req):
 def detail(req, w_id):
     w = get_object_or_404(word, id=w_id)
     return render(req, 'mydic/detail.html', {'w':w})
+
+def form(req):
+    return render(req, 'mydic/form.html')
+
+def submit(req):
+    nword = req.POST('nword')
+    ntype = req.POST('ntype')
+    ntrans = req.POST('ntrans')
+    ndate = timezone.now()
+
+    w = word(word_text=nword, word_type=ntype, word_trans=ntrans, word_date=ndate)
+    w.save()
+
+    return HttpResponseRedirect(reverse('mydic:index'))
