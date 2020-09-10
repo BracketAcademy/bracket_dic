00e175f746d763cbabe0223071b249691a34694d
Arman Hadi <armanhadi728@yahoo.com>
2020-09-10 11:39:37 +0430

    404

--- dic/mydic/views.py
+++ dic/mydic/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 # Create your views here.
@@ -7,7 +7,6 @@ def index(req):
     w = word.objects.order_by('word_text')
     return render(req, 'mydic/index.html', context={'word':w})
 
-
 def detail(req, w_id):
-    w = word.objects.get(id=w_id)
+    w = get_object_or_404(word, id=w_id)
     return render(req, 'mydic/detail.html', {'w':w})
