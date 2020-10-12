4f04575b3b64f92cc8fa216b85e444bb8cb02831
Arman Hadi <armanhadi728@yahoo.com>
2020-10-12 22:42:03 +0330

    user attached to words

--- dic/mydic/models.py
+++ dic/mydic/models.py
@@ -1,8 +1,11 @@
+# Import things
 from django.db import models
 from django.utils import timezone
-from datetime import timedelta
-# Create your models here.
+from django.contrib.auth.models import User
 
+from datetime import timedelta
+
+# Create your models here.
 
 class word(models.Model):
     word_text = models.CharField(
@@ -13,6 +16,8 @@ class word(models.Model):
     word_trans = models.CharField(
         max_length=200, verbose_name="Translation", null=False, blank=False)
     word_date = models.DateTimeField("Date Added")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def recent(self):
         return timezone.now() - timedelta(hours=12) <= self.word_date <= timezone.now()
