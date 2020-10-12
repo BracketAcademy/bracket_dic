5e9507a2e41986924c098d50e5bb1018867eccb3
Arman Hadi <armanhadi728@yahoo.com>
2020-10-12 21:52:03 +0330

    'settings'

--- dic/dic/settings.py
+++ dic/dic/settings.py
@@ -126,3 +126,8 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('Hey pal, it looks you are in production stage!')
